c596e4a4bbc2f1faf63f8a7d9438fed6bea1e794
Андрей Понамарчук <ponamarchuk.andrey@icloud.com>
2018-07-30 16:08:57 +0500

    Initial Commit

---
+++ How Many Fingers Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  How Many Fingers Game
+//
+//  Created by Андрей Понамарчук on 30/07/2018.
+//  Copyright © 2018 Андрей Понамарчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
