ef38720c80fe16a904a929ffe2cda88ae1bed2a6
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2018-08-20 22:14:23 +0500

    //

--- How Many Fingers Game/ViewController.swift
+++ How Many Fingers Game/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var usersAnswer: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    
+    @IBAction func checkAnswer(_ sender: UIButton) {
+        let randomFingers = arc4random_uniform(6)
+        if usersAnswer.text == String(randomFingers) {
+            result.text = "You're right!"
+        } else {
+            result.text = "Wrong! It was a \(randomFingers)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
